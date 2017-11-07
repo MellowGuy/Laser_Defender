441d1d9bade22518bc2720952855d434459ec123
Tyler R Thompson <thotyl88@gmail.com>
2017-11-06 20:41:30 -0500

    Script updates
    added to enemy spawner all scripts got unused namespaces removed.

--- Laser Defender Project/Assets/Scripts/EnemySpawner.cs
+++ Laser Defender Project/Assets/Scripts/EnemySpawner.cs
@@ -1,40 +1,31 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class EnemySpawner : MonoBehaviour
 {
+	private bool movingRight = false;
+
 	public GameObject enemyPrefab;
 	public float width = 10f;
 	public float height = 5f;
 	public float speed = 5f;
 	public float xMin;
 	public float xMax;
-
-	private bool movingRight = false;
+	public float spawnDelay = 5f;
 
 	// At start, runs foreach transform in the spawners transform, spawns enemy at that position
 	void Start()
 	{
 		float distanceToCamera = transform.position.z - Camera.main.transform.position.z;
-
+		//Calculates play space and sets left and right x axis boundaries
 		Vector3 leftBoundary = Camera.main.ViewportToWorldPoint(new Vector3(0, 0, distanceToCamera));
 		Vector3 rightBoundary = Camera.main.ViewportToWorldPoint(new Vector3(1, 0, distanceToCamera));
 
 		Debug.LogWarningFormat("LeftBounds:{0}|RightBounds:{1}", leftBoundary, rightBoundary);
 
-
 		xMin = leftBoundary.x;
 		xMax = rightBoundary.x;
-
-		Debug.LogFormat("Child Count: " + transform.childCount);
-
-		foreach (Transform item in transform)
-		{
-			GameObject enemy = Instantiate(enemyPrefab, item.transform.position, Quaternion.identity) as GameObject;
-			enemy.transform.parent = item;
-			Debug.LogFormat("Enemy Spwaned at: {0}, {1}.", item.position.x, item.position.y);
-		}
+		
+		SpawnUntilFull();
 	}
 
 
@@ -55,20 +46,82 @@ public class EnemySpawner : MonoBehaviour
 		float leftEdgeOfFormation = transform.position.x - (0.5f * width);
 		float rightEdgeOfFormation = transform.position.x + (0.5f * width);
 
-
+		//swaps moving right back and forth
 		if (leftEdgeOfFormation < xMin)
-		{
 			movingRight = true;
+		
+		else if (rightEdgeOfFormation > xMax)
+			movingRight = false;
+		
+		//Checks if all enemies are dead.
+		if (AllMembersDead())
+		{
+			Debug.Log("ALl enemies dead.");
+			SpawnUntilFull();
+		}
+	}
+
+
+
+	//returns a position if it's child is empty
+	Transform NextFreePosition()
+	{
+		foreach (Transform childPositionGameObject in transform)
+		{
+			if (childPositionGameObject.childCount == 0)
+				return childPositionGameObject;
+		}
+		return null;
+	}
+
+	//spawns enemies at next free position. Method calls itself recursively if there's a free position after spawnDelay.
+	void SpawnUntilFull()
+	{
+		Transform freePosition = NextFreePosition();
+
+		if (freePosition != null)
+		{
+			GameObject enemy = Instantiate(enemyPrefab, freePosition.transform.position, Quaternion.identity) as GameObject;
+			enemy.transform.parent = freePosition;
+			Debug.LogFormat("Enemy Spwaned at: {0}, {1}.", freePosition.position.x, freePosition.position.y);
 		}
 
-		else if (rightEdgeOfFormation > xMax)
+		if (NextFreePosition() != null)
 		{
-			movingRight = false;
+			Invoke("SpawnUntilFull", spawnDelay * Time.deltaTime);
 		}
+	}
+
+
+	public bool AllMembersDead()
+	{
+		foreach (Transform childPositionGameObject in transform)
+		{
+			if (childPositionGameObject.childCount > 0)
+			{
+				return false;
+			}
+		}
+
+		return true;
 	}
 
 	public void OnDrawGizmos()
 	{
 		Gizmos.DrawWireCube(transform.position, new Vector3(width, height));
 	}
+
+
+
+	/*
+public void SpawnAllEnemies()
+{
+	foreach (Transform item in transform)
+	{
+		GameObject enemy = Instantiate(enemyPrefab, item.transform.position, Quaternion.identity) as GameObject;
+		enemy.transform.parent = item;
+		Debug.LogFormat("Enemy Spwaned at: {0}, {1}.", item.position.x, item.position.y);
+	}
+}
+*/
 }
