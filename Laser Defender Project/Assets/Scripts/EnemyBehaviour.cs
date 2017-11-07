eb5a0bc1f59a181f4e83d9fda8d97018977db45e
Tyler R Thompson <thotyl88@gmail.com>
2017-11-06 23:06:56 -0500

    Added starfield and score board
    Added partical effects as a starfield, created scoreboard that updates each time enemy is hit.

--- Laser Defender Project/Assets/Scripts/EnemyBehaviour.cs
+++ Laser Defender Project/Assets/Scripts/EnemyBehaviour.cs
@@ -6,7 +6,17 @@ public class EnemyBehaviour : MonoBehaviour
 	public float laserSpeed = 10f;
 	public GameObject laserShot;
 	public float shotsPerSecond = 0.5f;
+	public int scoreValue = 1000;
 
+	private ScoreKeeper scoreKeeper;
+
+	//Gets reference to text object "Score" dynamically. 
+	private void Start()
+	{
+		scoreKeeper = GameObject.Find("Score").GetComponent<ScoreKeeper>();
+	}
+
+	//Randomizes shooting of enemies
 	private void Update()
 	{
 		float probability = Time.deltaTime * shotsPerSecond;
@@ -16,20 +26,26 @@ public class EnemyBehaviour : MonoBehaviour
 		}
 	}
 
+	//FireLaser creates laser game object w/ velocity
 	void FireLaser()
 	{
 		GameObject enemyShot = Instantiate(laserShot, new Vector3(transform.position.x, transform.position.y - 1f, 0f), Quaternion.identity, transform.parent) as GameObject;
 		enemyShot.GetComponent<Rigidbody2D>().velocity = new Vector2(0, -laserSpeed);
 	}
 
+	//Collisions with this enemy
 	private void OnTriggerEnter2D(Collider2D collision)
 	{
+		//gets reference to LaserFire type object if is collision
 		LaserFire laser = collision.gameObject.GetComponent<LaserFire>();
 
-		if (laser)
+		//if not null object
+		if (laser != null)
 		{
+			//calls Hit() of the laser, gets damage amount and ups the score by enemy scoreValue
 			laser.Hit();
 			health -= laser.GetDamage();
+			scoreKeeper.Score(scoreValue);
 
 			if (health <= 0)
 			{
---
+++ Laser Defender Project/Assets/Scripts/ScoreKeeper.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreKeeper : MonoBehaviour
+{
+	public int score = 0;
+	private Text myText;
+
+	//Initializes the score
+	private void Start()
+	{
+		myText = GetComponent<Text>();
+		Score(0);
+	}
+
+	//add to score function
+	public void Score(int points)
+	{
+		score += points;
+		myText.text = "Game Score: " + score.ToString();	
+	}
+
+	////not currently used
+	//public void Reset()
+	//{
+	//	score = 0;
+	//}
+}
